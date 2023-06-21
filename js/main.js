67fa4d0b131f1577224146b94ec5ac10a5292aaa
liyan-nk <117467536+liyan-nk@users.noreply.github.com>
2023-06-21 16:30:40 +0530

    admission page

--- js/main.js
+++ js/main.js
@@ -152,3 +152,19 @@ $(document).ready(function(){
         }
     })
 })
+
+$('[data-type="adhaar-number"]').keyup(function() {
+    var value = $(this).val();
+    value = value.replace(/\D/g, "").split(/(?:([\d]{4}))/g).filter(s => s.length > 0).join("-");
+    $(this).val(value);
+  });
+  
+  $('[data-type="adhaar-number"]').on("change, blur", function() {
+    var value = $(this).val();
+    var maxLength = $(this).attr("maxLength");
+    if (value.length != maxLength) {
+      $(this).addClass("highlight-error");
+    } else {
+      $(this).removeClass("highlight-error");
+    }
+  });
